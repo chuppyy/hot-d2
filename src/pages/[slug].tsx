a133f39e06e067c3724d470f43b28133ddc76a32
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-05-08 17:05:03 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -12,7 +12,7 @@ export default function Page(data: any) {
     try {
       var qcImgDiv = document.getElementById("qcImg");
 
-      if (qcImgDiv) {
+      if (qcImgDiv && 0) {
         var insElement = document.createElement("ins");
         insElement.className = "adsbygoogle";
         insElement.style.display = "block";
