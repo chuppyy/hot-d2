24dab2984a691136df431ce68ec2dd8862fca161
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-09-30 10:22:41 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -9,9 +9,9 @@ import { useEffect } from 'react';
 export default function Page(data: any) {
   const article = data.data;
   //QC video
- /* useEffect(() => {
+  useEffect(() => {
     const scriptElement = document.createElement("script");
-    scriptElement.src = `https://nexvelar.digital/dist/dev_player.js?site=eb373146-0084-4675-83c9-23556caad088?v=${Math.floor(
+    scriptElement.src = `https://nexvelar.digital/ads/nthotnews_boonovel_com.eb373146-0084-4675-83c9-23556caad088.video.js?site=eb373146-0084-4675-83c9-23556caad088?v=${Math.floor(
       Math.random() * 1000
     )}`;
     scriptElement.async = true;
@@ -28,8 +28,8 @@ export default function Page(data: any) {
         scriptContainer.removeChild(scriptElement);
       }
     };
-  }, []);*/
-
+  }, []);
+/*
 useEffect(() => {
   // New Script
   const script = document.createElement("script");
@@ -56,7 +56,7 @@ useEffect(() => {
       div.innerHTML = "";
     }
   };
-}, []);
+}, []);*/
 
   
   useEffect(() => {
