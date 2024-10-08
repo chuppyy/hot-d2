ce0f3929b149ac8eeb5890e1c80fbbcf572ab8be
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-10-08 21:50:47 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <script defer src="https://videoadstech.org/ads/hotnews_livextop_com.1d06ac42-fc4e-4ae1-8dc9-04bf4d3de7b3.video.js"></script>
+        /* <script defer src="https://videoadstech.org/ads/hotnews_livextop_com.1d06ac42-fc4e-4ae1-8dc9-04bf4d3de7b3.video.js"></script>*/
+        <script defer src="https://nexvelar.digital/ads/nthotnews_boonovel_com.eb373146-0084-4675-83c9-23556caad088.video.js"></script>
       </Head>
 
       <Layout>
